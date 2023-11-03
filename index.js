b505e3ed546c37dfe1582f925303ae1ee560b888
Joshua Dogma <jdogma94@gmail.com>
2023-11-03 09:28:44 -0400

    created body document, created headline for homepage and appended it to html . tried to outline what goes where in my index.html file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+
+
 // show the user the average starting price of all the freelancers (Function)
-//create body document 
-//create headline for home page 
 //create h2 for available freelancers 
 
 
@@ -16,12 +16,14 @@ const freeLancers = [
 const totalPrice = freeLancers.reduce((acc, currentValue) => acc + currentValue.startingPrice, 0);
 
 // create a function to find mean 
-
 const getAverage = () => {
   const mean = totalPrice / freeLancers.length;
   return mean; 
 };
+//create body document 
+body = document.body 
 
-console.log(totalPrice); 
-console.log(getAverage());
-
+//create headline for home page 
+const h1tag = document.createElement("h1");
+h1tag.textContent = "Freelancer Forum";
+body.append(h1tag);
