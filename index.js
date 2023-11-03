4742ffc16350b1fcb66a75883f5aec4a06a1b765
Joshua Dogma <jdogma94@gmail.com>
2023-11-03 08:19:49 -0400

    Found the sum using reduce and found the mean. Still only visible in console log

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-//Create an intial array named freeLancers
 // show the user the average starting price of all the freelancers (Function)
 //create body document 
 //create headline for home page 
 //create h2 for available freelancers 
 
-const freeLancers = [
+
+
+//Create an intial array named freeLancers
+const freeLancers = [                 
   {name: "Alice", occupation:"Writer", startingPrice: 30},
   {name: "Bob", occupation:"Teacher", startingPrice: 50},
   {name: "Carol", occupation:"Programmer", startingPrice: 70}  
 ]
 
-console.log(freeLancers);
+// Use reduce to find sum of starting price 
+const totalPrice = freeLancers.reduce((acc, currentValue) => acc + currentValue.startingPrice, 0);
+
+// create a function to find mean 
+
+const getAverage = () => {
+  const mean = totalPrice / freeLancers.length;
+  return mean; 
+};
+
+console.log(totalPrice); 
+console.log(getAverage());
 
