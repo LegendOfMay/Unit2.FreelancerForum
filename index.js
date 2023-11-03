60154a82a17555fa2af7c0a9eeb45e6c8d3ffcfc
Joshua Dogma <jdogma94@gmail.com>
2023-11-03 07:25:31 -0400

    wrote pseudo code. created a array named freeLancers.

---
+++ index.js
@@ -0,0 +1,14 @@
+//Create an intial array named freeLancers
+// show the user the average starting price of all the freelancers (Function)
+//create body document 
+//create headline for home page 
+//create h2 for available freelancers 
+
+const freeLancers = [
+  {name: "Alice", occupation:"Writer", startingPrice: 30},
+  {name: "Bob", occupation:"Teacher", startingPrice: 50},
+  {name: "Carol", occupation:"Programmer", startingPrice: 70}  
+]
+
+console.log(freeLancers);
+
