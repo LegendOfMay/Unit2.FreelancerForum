1c2f4e0dcf6f4229e0921aa5aacc4736cf8e2b40
Joshua Dogma <jdogma94@gmail.com>
2023-11-03 10:05:49 -0400

    Added h2 header, created CSS file for reference

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-
-
-
-//create h2 for available freelancers 
-
-
-
 //Create an intial array named freeLancers
 const freeLancers = [                 
   {name: "Alice", occupation:"Writer", startingPrice: 30},
@@ -34,4 +27,10 @@ h1tag.setAttribute("style", "margin:auto; width:50%; text-align:center;");
 const functionSection = document.createElement("section");
 functionSection.textContent = `The average starting price is $ ${getAverage()}.`;
 body.append(functionSection);
-functionSection.setAttribute("style", "margin:auto; width:50%; text-align:center; text-decoration:underline; font-size: larger;");
+functionSection.setAttribute("style", "margin:auto; width:50%; text-align:center; text-decoration:underline; font-size: larger; padding: 30px;");
+
+//create h2 for available freelancers 
+const h2tag = document.createElement("h2");
+h2tag.textContent = "Available Freelancers";
+body.append(h2tag);
+h2tag.setAttribute("style", "margin:auto; width:50%; text-align:center; padding: 30px;");
